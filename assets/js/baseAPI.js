4d09756e58aaaef63f7d5233c2628ee66d5197b1
球球 <623903979@qq.com>
2021-05-24 09:29:28 +0800

    新增了一个bug

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -12,7 +12,11 @@ $.ajaxPrefilter(function (opitons) {
     opitons.complete = function (res) {
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
             localStorage.removeItem('token');
-            location.href = '/login.html';
+            if (window.parent) {
+                window.parent.location.href = '/login.html';
+            } else {
+                location.href = '/login.html';
+            }
         }
     }
 })
